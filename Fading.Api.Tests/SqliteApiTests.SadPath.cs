96bf5f0e46d4a1f6626dca2f29c2b3045c57d003
“Mathias <Mathias.Viklund@gmail.com>
2022-03-16 10:45:24 +0100

    test rejection to create an empty post

--- Fading.Api.Tests/SqliteApiTests.SadPath.cs
+++ Fading.Api.Tests/SqliteApiTests.SadPath.cs
@@ -2,4 +2,19 @@ namespace Fading.Api.Tests;
 
 public partial class SqliteApiTests
 {
+  [Fact]
+  public async void Should_Reject_New_Post_Without_content()
+  {
+    // Arrange
+    using var context = CreateContext();
+    var controller = new MessageController(context);
+    
+    // Act
+    var messagesTask = await controller.NewMessage(null);
+    var result = messagesTask.Value;
+
+    // Assert
+    result!.Should().BeNull();
+    messagesTask.Result.Should().BeOfType<BadRequestObjectResult>();
+  }
 }
